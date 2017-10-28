60c2ff996470d1abd274f951ce09056c8f77904c
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-28 11:00:07 -0700

    Initial Commit

---
+++ Pointers and References/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pointers and References
+//
+//  Created by Katherine Reinhart on 10/28/17.
+//  Copyright © 2017 reinhart.digital. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
